e3635a7965927ede1eb1f9a63bb918255834b735
borisHP <borisandres.guinez@gmail.com>
2024-10-09 21:28:33 -0300

    add aplicacion vuejs formulario bindig

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import Tarjeta from './components/Tarjeta.vue'
+</script>
+
+<template>
+  <Tarjeta />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
